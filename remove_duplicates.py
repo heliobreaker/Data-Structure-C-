9197a47f73dacfdf04b964ba8380c957330b0555
sakibmuhtadee <sakib0911@gmail.com>
2021-05-06 12:32:27 +0600

    Leetcode 27

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -3,6 +3,28 @@
 """
 Created on Thu May  6 09:52:39 2021
 
-@author: heliobreaker
+@author: md.sakib muhtadee
 """
+class Solutions:
+    def __init__(self,arr,needsorting=True):
+        if (needsorting==True) :
+            self.arr=sorted(arr)
+        else:
+            self.arr=arr
 
+    def remove_Duplicates(self):
+        arr=self.arr
+        j=0
+        
+        for i in range (0,len(arr)):
+            
+            if arr[i]!=arr[i-1]:
+                arr[j]=arr[i]
+                j=j+1
+        
+        return arr[:j]
+    
+if __name__=="__main__":
+    nums=[1,3,1,2,3,8,3,4,10,4,5,9,5,6,7]
+    S=Solutions(nums)
+    print(S.remove_Duplicates())
